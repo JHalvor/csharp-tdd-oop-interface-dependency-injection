2104ca35fb920791f2f06ff2b0b8e77d67b65691
Jonas <jonashalvorsen2013@live.no>
2024-08-22 13:37:27 +0200

    Exercise-complete

--- tdd-oop-interface-dependency-injection.CSharp.Main/Program.cs
+++ tdd-oop-interface-dependency-injection.CSharp.Main/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tdd_oop_interface_dependency_injection.CSharp.Main;
+using tdd_oop_interface_dependency_injection.CSharp.Main.Alphabets;
+
+English english = new English();
+string englishWord = "Hello";
+
+Greek greek = new Greek();
+string greekWord = "γεια";
+
+Russian russian = new Russian();
+string russianWord = "привет";
+
+Scrabble scrabble = new Scrabble(russian);
+
+Console.WriteLine(scrabble.score(russianWord));
+
+
+
